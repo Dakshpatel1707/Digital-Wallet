fe32d1c8e2c5c584dac64b372b84fd61add4e111
Dakshpatel1707 <pateldaksh17072006@gmail.com>
2026-06-20 09:39:08 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+  baseURL: "https://digital-wallet-dr4k.onrender.com/api",
 });
 
 export default api;
